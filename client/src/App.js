06df0668b8029f0b740e87e9e9ff1f50f7ff00db
Sven Verbeek <verbeek6@hotmail.com>
2023-05-25 17:13:27 +0200

    $1

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Link to="/">Home</Link>
+        <Link to="/otherpage">Other Page link</Link>
+      </header>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Fib/>}/>
+          <Route exact path="/otherpage" element={<OtherPage/>}/>
+        </Routes>
+      </div>
+    </div>
+    </Router>
+  );
+};
+
+export default App;
