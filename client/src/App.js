b367f53853b966fc41713d5f4c4ef0a51ab541c0
SvenVerbeek <verbeek6@hotmail.com>
2023-08-14 10:42:39 +0200

    update with new code

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
 
@@ -9,20 +9,26 @@ function App() {
   return (
     <Router>
       <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Link to="/">Home</Link>
-        <Link to="/otherpage">Other Page link something...</Link>
-      </header>
-      <div>
-        <Routes>
-          <Route exact path="/" element={<Fib/>}/>
-          <Route exact path="/otherpage" element={<OtherPage/>}/>
-        </Routes>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
+        </header>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
       </div>
-    </div>
     </Router>
   );
-};
+}
 
 export default App;
